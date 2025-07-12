64fb79db2e486ed570654b9d3e7008bde84dd7e7
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2025-07-12 19:24:29 +0200

    Update script.js

--- First/script.js
+++ First/script.js
@@ -1,4 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-    //alert("Üdvözöllek az oldalon!"); // Egy felugró üzenet az oldal betöltésekor
+document.addEventListener("DOMContentLoaded", function () {
+  //alert("Üdvözöllek az oldalon!"); // Egy felugró üzenet az oldal betöltésekor
 });
