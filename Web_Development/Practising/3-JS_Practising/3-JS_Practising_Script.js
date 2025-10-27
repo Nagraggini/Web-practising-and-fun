ef53ff95cd8999cca3d5c43b99959fcbdcc67d24
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2025-10-27 19:11:40 +0100

    JS data types

--- Web_Development/Practising/3-JS_Practising/3-JS_Practising_Script.js
+++ Web_Development/Practising/3-JS_Practising/3-JS_Practising_Script.js
@@ -5,3 +5,18 @@ a = 1;
 //Implicit érték adás.
 var b = 2;
 alert(a + b);
+
+//Adattípusok:
+var c = 3; //number
+var d = 3.5; //float
+
+var a = "Hello"; // dupla idézőjel
+var b = "Hello"; // szimpla idézőjel
+//A backtick (`) speciális, mert enged változóbeillesztést és többsoros szöveget
+let name = "Andrea";
+var c = `Hello`; // backtick (templateliterál)
+
+let message = `Hello, ${name}!`; // → "Hello, Andrea!"
+var g = true; //boolean
+var i = null; //null
+var j; //undefined
