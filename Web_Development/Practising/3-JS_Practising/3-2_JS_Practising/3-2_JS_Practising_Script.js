a9268fb8a9daf7b015a4380106438eb04c4dbeee
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2025-11-29 16:30:31 +0100

    Practising JS.

--- Web_Development/Practising/3-JS_Practising/3-2_JS_Practising/3-2_JS_Practising_Script.js
+++ Web_Development/Practising/3-JS_Practising/3-2_JS_Practising/3-2_JS_Practising_Script.js
@@ -13,8 +13,8 @@ console.warn('Lehet hiba');
 
 /*var: Változók újradeklarációja – Amikor a var változót használjuk, ugyanazt a változónevet többször is beírhatjuk a hatókörön belül, mivel az új változó felülírja az előzőt. Az újradeklaráció problémája – Tudtán kívül deklarálhatunk egy másik változót ugyanazzal a névvel, ami nem kívánt eredményt adhat.*/
 
-let kor1=20;
-kor1=21;
+let kor1 = 20;
+kor1 = 21;
 
 //console.log(kor1);
 
@@ -26,23 +26,23 @@ console.log(kor1); //Hibás, mert a const sohasem változik meg.
 
 // -- Adat típusok
 //Primitívek:
-const nev='Zsolt'; //String
-const kor3=21; //Number
-const suly=80.5; //Number
-const felnott=true; //Boolean
+const nev = "Zsolt"; //String
+const kor3 = 21; //Number
+const suly = 80.5; //Number
+const felnott = true; //Boolean
 
 //console.log(nev, kor3,suly,felnott);
 
 //Objektumok:
-const date=new Date(); //Mai dátum.
-const date2=new Date('2025-11-24');
+const date = new Date(); //Mai dátum.
+const date2 = new Date("2025-11-24");
 
 /*console.log(date);
 console.log(date2);
 */
 
-const x=null; // Szándékosan „nincs értéke” — üresre állított változó.
-const y= undefined; // Definiálva van, de nincs hozzárendelve konkrét érték.
+const x = null; // Szándékosan „nincs értéke” — üresre állított változó.
+const y = undefined; // Definiálva van, de nincs hozzárendelve konkrét érték.
 
 /*
 null → te adod meg, hogy nincs értéke.
@@ -109,4 +109,87 @@ console.log(date.toISOString()); // Ki írja ISO formátumban a dátumot.
 
 // -- Feltételek
 
-const x1=null;
+const x1 = 14;
+const x2 = 10;
+
+/*
+if (x1 <= x2) {
+    console.log("Igaz.");
+} else {
+    console.log("Hamis.");
+}
+
+if (x1 === x2) { //Összehasonlítja a típust is.
+    console.log("Igaz.");
+} else {
+    console.log("Hamis.");
+}
+    */
+
+/*
+if (x1 == 12) {
+    //Összehasonlítja a számokat.
+    console.log("Igaz.");
+} else if (x1 == 13) {
+    console.log("Hamis.");
+} else {
+    console.log("Más");
+}
+
+switch (x1) {
+    case 10:
+        console.log("10");
+        break; //Ezzel megállítjuk a ciklus futását.
+    case 11:
+        console.log("11");
+        break;
+    default:
+        console.log("Hamis.");
+}
+        */
+
+/*
+if (x1 != x2) {
+    //Összehasonlítja a számokat.
+
+    console.log("Igaz.");
+} else {
+    console.log("Hamis.");
+}
+
+if (!!true == false) {
+    console.log("IGAZZ");
+} else {
+    console.log("HAMISS");
+}
+*/
+
+/*
+if (!0) {
+    console.log("IGAZZZ");
+} else {
+    console.log("HAMISSS"); // 0 . !1
+}
+
+//Rövidítések:
+
+//delkaráció = if ()    {}    else {}
+const color = x1 > 10 ? "piros" : "kek";
+console.log(color);
+*/
+
+//Tömbök (vagyis objektumok, array-ek)
+
+//Mivel a js nem típusos nyelv, ezért lehet szám is a tömbben.
+const allatok = ["kutya", "macska", "ló", 10, true];
+console.log(allatok[0]);
+
+//console.log(Array.isArray(allatok));
+console.log(allatok.length);
+
+allatok[6] = "kacsa";
+
+console.log(allatok);
+
+const teszt = !null ? "igaz" : "hamis";
+console.log(Math.ceil(4.4));
