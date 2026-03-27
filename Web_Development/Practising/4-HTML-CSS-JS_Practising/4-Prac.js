6fd15f9b16c44805ecfb2a230631170f1188370b
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-03-27 16:54:22 +0100

    Practising JS.

--- Web_Development/Practising/4-HTML-CSS-JS_Practising/4-Prac.js
+++ Web_Development/Practising/4-HTML-CSS-JS_Practising/4-Prac.js
@@ -1,14 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const title = document.getElementById("page_title");
+    title.innerText = "Welcome";
+    //====================== 0. feladat ============================
+    //Egy oldalon egy ID-t csak egyszer szabad használni, így ez a parancs mindig pontosan egy elemet ad vissza.
+    const elsoGomb = document.getElementById("elsoGomb");
+
+    elsoGomb.addEventListener("click", () => {
+        //A gomb szövegét módosítjuk.
+        elsoGomb.innerText = "Hahó! Megváltozott a szöveg.";
+    });
+
+    //Ha több ilyen osztályú elem is van, a querySelector csak a legelsőt fogja kiválasztani.
+    const masodikGomb = document.querySelector(".masodikGomb");
+    const kiemeltCikk = document.querySelector("kiemeltCikk");
+
+    masodikGomb.addEventListener("click", () => {
+        masodikGomb.innerText = "Hello";
+        console.log("Kiemelt cikk: " + kiemeltCikk);
+    });
+
+    //====================== 1. feladat ============================
     // 1. Gomb lekérése
-    const gomb = document.querySelector("#gomb");
-    console.log("A gomb szövege: " + gomb.innerText);
+    const harmadikGomb = document.querySelector("#harmadikGomb");
+    console.log("A gomb szövege: " + harmadikGomb.innerText);
 
     // 2. Gomb színének változtatása click eseményre
-    gomb.addEventListener("click", (event) => {
-        console.log(event.target.innerText);
+    harmadikGomb.addEventListener("click", (event) => {
+        console.log("A gomb szövege target-el: " + event.target.innerText);
         event.target.style.backgroundColor = "blue";
     });
 
+    //====================== 3. feladat ============================
     // 3. Színválasztó input
     const kivalasztottSzinInput = document.getElementById("kivalasztottSzin");
     const szinMutatasa = document.getElementById("szinMutatasaLabel");
@@ -23,4 +45,19 @@ document.addEventListener("DOMContentLoaded", () => {
     // 5. Kézi alapértelmezett érték beállítása
     kivalasztottSzinInput.value = "#448e88"; // lila
     //szinMutatasa.textContent = `Kiválasztott szín hex kódja: ${kivalasztottSzinInput.value}`;
+
+    //====================== 4. feladat ============================
+
+    //A querySelectorAll nem egyetlen elemet ad vissza, hanem egy NodeList-et (olyan, mint egy tömb/lista).
+    const ingredientsLista = document.querySelectorAll("ul.ingredients li");
+    const negyedikGomb = document.getElementById("negyedikGomb");
+
+    negyedikGomb.addEventListener("click", () => {
+        // Egy ciklussal végigmegyünk a lista minden egyes elemén
+        ingredientsLista.forEach((e) => {
+            e.style.backgroundColor = "blue";
+            e.style.color = "white";
+            ingredientsLista[0].textContent = "Teljeskiőrlésű liszt";
+        });
+    });
 });
