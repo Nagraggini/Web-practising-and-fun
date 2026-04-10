9abb21253b3d81c64d6b12acfb35001176442206
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-04-10 17:58:54 +0200

    Reorganising folder structure.

---
+++ assets/js/loadComponents.js
@@ -0,0 +1,25 @@
+fetch("./components/header.html")
+    .then((res) => res.text())
+    .then((data) => {
+        document.getElementById("header").innerHTML = data;
+        console.log("Betöltött a header.");
+    });
+
+/*Ez a JavaScript kód dinamikusan frissíti egy HTML oldalon az aktuális évet egy elemben,
+         jellemzően lábjegyzetekben vagy copyright szövegekben, így nem kell kézzel módosítani minden év elején.*/
+function setYear() {
+    console.log("Beállítjuk az évszámot.");
+    const yearEl = document.getElementById("year");
+    if (yearEl) yearEl.textContent = new Date().getFullYear();
+}
+
+window.addEventListener("load", setYear);
+
+fetch("./components/footer.html")
+    .then((res) => res.text())
+    .then((data) => {
+        document.getElementById("footer").innerHTML = data;
+        console.log("Betöltött a footer");
+    });
+
+// TODO: Elkell menteni a téma választást localStorageba.
