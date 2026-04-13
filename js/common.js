700b8fd6a1a26ee7ae689481d5be0c97596e8cb8
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-04-13 23:09:56 +0200

    refactor: set up MVC structure for index JS files

--- js/common.js
+++ js/common.js
@@ -1,1 +1,13 @@
-// TODO: El kell menteni a téma választást localStorageba.
+/*Ez az a fájl, amit minden HTML oldalad behív. Ez mondja meg, hogy az adott oldalon mi történjen.*/
+
+// Alapvető betöltő funkciók.
+import "./loader.js";
+
+// UI Komponensek logikája
+import "./components/firework-ui.js";
+import "./components/footer-script.js";
+import "./components/header-script.js";
+import "./components/loader.js";
+import "./components/model.js";
+
+console.log("Minden js modul sikeresen betöltve!");
---
+++ js/components/loader.js
@@ -0,0 +1,1 @@
+/*Ebben csak egyetlen technikai eszköz van: egy függvény, ami képes egy külső HTML fájlt (mint a header) belerakni egy üres dobozba.*/
