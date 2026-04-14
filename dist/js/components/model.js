c124241b75596a38fbf514e434c12e37113f14f3
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-04-14 19:02:34 +0200

    setup: html build process and github actions deploy (with node.js)

---
+++ dist/js/components/model.js
@@ -0,0 +1,4 @@
+/*A programozásban (főleg az MVC – Model-View-Controller mintánál) a Model tárolja az adatokat és az üzleti logikát.*/
+//Adattároló
+
+// TODO: El kell menteni a téma választást localStorageba.
