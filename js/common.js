c75e7e88bd9a101372c9290b8d5d86dcbe1d8b4c
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-04-15 23:01:48 +0200

    Remove module js on index site

--- js/common.js
+++ js/common.js
@@ -1,13 +1,3 @@
-/*Ez az a fájl, amit minden HTML oldalad behív. Ez mondja meg, hogy az adott oldalon mi történjen.*/
-console.log("common.js eleje");
-// UI Komponensek logikája
-import "./components/firework-ui.js";
-
-import "./components/loader.js";
-import "./components/model.js";
-
-console.log("common.js vége");
-
 //Mobilos navigációs bár mutatása és elrejtése.
 function showSidebar() {
     const sidebar = document.querySelector(".sidebar");
